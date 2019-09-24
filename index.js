3687f8c036d6b59ac568695932e0a27ad0a3a0a4
Heidi B <boulter.heidi@gmail.com>
2019-09-24 21:08:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipes = {key: 'value'}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, { [key]: value })
-}
-
 function deleteFromObjectByKey(object, key) {
   var object = recipes
   delete object[key];
@@ -18,4 +14,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
   return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, recipes, { [key]: value })
 }
