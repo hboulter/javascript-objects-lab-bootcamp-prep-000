becd2a7dcd777277903786c1a685b20e66d3d37f
Heidi B <boulter.heidi@gmail.com>
2019-09-24 20:52:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {key: 'value'}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, recipes, { [key]: value })
 }
+
+function deleteFromObjectByKey(object, key) {
+  var object = recipes
+  delete object.key;
+}
